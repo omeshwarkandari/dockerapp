64dccbb473e3496bbef786448e753f7742b07d10
omeshwar <73010142+omeshwarkandari@users.noreply.github.com>
2022-09-14 10:47:07 +0530

    Update DockerappApplication.java

--- src/main/java/com/dockerapp/DockerappApplication.java
+++ src/main/java/com/dockerapp/DockerappApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootApplication
@@ -18,5 +20,8 @@ public class DockerappApplication {
 	@GetMapping("/")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+	@Test
+	void contextLoads() {
 	}
 }

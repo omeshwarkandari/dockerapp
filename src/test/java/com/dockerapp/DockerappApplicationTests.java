cbffda65ef47fac93b431c0c64230ead1971d4ce
omeshwar <73010142+omeshwarkandari@users.noreply.github.com>
2022-09-23 16:01:45 +0530

    Update DockerappApplicationTests.java

--- src/test/java/com/dockerapp/DockerappApplicationTests.java
+++ src/test/java/com/dockerapp/DockerappApplicationTests.java
@@ -3,7 +3,7 @@ package com.dockerapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-public class DockerappApplicationTests {
+class DockerappApplicationTests {
 
 	@Test
 	public void test() {

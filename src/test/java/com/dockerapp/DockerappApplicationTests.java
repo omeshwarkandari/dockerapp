d86585e74a61800d21309519b5e9638051b86392
omeshwar <73010142+omeshwarkandari@users.noreply.github.com>
2022-09-23 16:45:44 +0530

    Update DockerappApplicationTests.java

--- src/test/java/com/dockerapp/DockerappApplicationTests.java
+++ src/test/java/com/dockerapp/DockerappApplicationTests.java
@@ -2,24 +2,12 @@ package com.dockerapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.dockerapp.entites.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+
 
 class DockerappApplicationTests {
 	
  
-// Interface
-// Extends JpaRepository
-	
-public interface PersonRepo
-	    extends JpaRepository<Person, Integer> {
-	    @Query(
-		"SELECT CASE WHEN COUNT(s) > 0 THEN TRUE ELSE FALSE END FROM Person s WHERE s.personId = ?1")
-	    Boolean
-	    isPersonExitsById(Integer id);
-    	   }
-
 	@Test
 	void test() {
 		System.out.println("TEST WAS SUCCESSFUL");

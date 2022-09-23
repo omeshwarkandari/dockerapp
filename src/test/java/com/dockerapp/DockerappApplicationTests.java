d29364ccb1fd2c8f70f083038372cba765f01aab
omeshwar <73010142+omeshwarkandari@users.noreply.github.com>
2022-09-23 16:24:22 +0530

    Update DockerappApplicationTests.java

--- src/test/java/com/dockerapp/DockerappApplicationTests.java
+++ src/test/java/com/dockerapp/DockerappApplicationTests.java
@@ -2,12 +2,27 @@ package com.dockerapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.company.CompareToTester;
+
 
 class DockerappApplicationTests {
+	
+	@Test
+	public void testDoSomething() {
+	  MyClass myClass = new MyClass();
+	  assertNull(myClass.doSomething());  // JUnit assertion
+	  assertThat(myClass.doSomething()).isNull();  // Fest assertion
+	}
+
+	@Test
+	public void testDoSomethingElse() {
+	  MyClass myClass = new MyClass();
+	  new CompareToTester().compareWith(myClass);  // Compliant - custom assertion method defined as rule parameter
+	  CompareToTester.compareStatic(myClass);  // Compliant
+	}
 
 	@Test
 	void test() {
 		System.out.println("TEST WAS SUCCESSFUL");
 	}
-
 }
